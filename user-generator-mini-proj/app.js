5ba91969a8fa3142dbc297dfa987a84a9e0bab07
wyne-ybanez <wyne_01@icloud.com>
2022-07-01 18:41:25 +0100

    Add getRandomUser method, add reactive styling classes

--- user-generator-mini-proj/app.js
+++ user-generator-mini-proj/app.js
@@ -13,6 +13,24 @@ const app = Vue.createApp({
       picture: 'https://randomuser.me/api/portraits/men/11.jpg',
     }
   },
+  /* 
+   We can write our methods underneath our data object
+   We can access our data in this, using the 'this' keyword
+  */
+  methods: {
+    async getRandomUser() {
+      const res = await fetch('https://randomuser.me/api')
+      const { results } = await res.json()
+
+      console.log(results)
+
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
+    },
+  },
 })
 
 // Mounting the App
