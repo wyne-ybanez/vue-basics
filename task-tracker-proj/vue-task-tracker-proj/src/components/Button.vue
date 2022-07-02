3970d042870cdfbc1be641c7dea1814410871681
wyne-ybanez <wyne_01@icloud.com>
2022-07-02 19:52:14 +0100

    Add data array, Button, Task components, Modified Header component

---
+++ task-tracker-proj/vue-task-tracker-proj/src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button 
+        :style="{background: color}" 
+        @click="Add()"
+        class="btn"
+        >   
+        {{text}}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: {
+                type: String,
+                default: 'Button',
+            },
+            color: {
+                type: String,
+            }
+        },
+        methods: {
+            Add() {
+                console.log('Added');
+            },
+            Update() {
+                console.log('Update');
+            },
+            Delete() {
+                console.log('Delete');
+            }
+        }
+    }
+</script>
---
+++ task-tracker-proj/vue-task-tracker-proj/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <!-- Accessing props through curly braces -->
+        <h1>{{title}}</h1>
+
+        <!-- Button Component -->
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+    import Button from './Button';
+
+    export default {
+        name: 'Header',
+        props: {
+            /* 
+            Passing in props in the form of an object
+            Props can also be objects with type and default values for more control
+            */
+            title: {
+                type: String,
+                default: 'Hello World',
+            },
+        },
+        components: {
+            Button,
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
